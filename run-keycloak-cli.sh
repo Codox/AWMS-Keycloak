134e24a7efac8c49541e16959f8dccfdf60498fb
Connor Brady <connorbrady@codox.eu>
2023-04-08 21:06:51 +0100

    UPDATED Run script to substitute

--- run-keycloak-cli.sh
+++ run-keycloak-cli.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 # Extract user specified variables
+config=/config/awms-realm
 file=/misc/keycloak-cli-config.json
 variables=$(jq -rj '. | to_entries[] | "\(.key)=\(.value)\n"' $file)
 
-echo $variables
+# echo $variables
 
 # printf $variables
 
 while IFS= read -r line; do
-    echo $line
+    # echo $line
     key=$(echo "$line" | cut -d= -f1)
     value=$(echo "$line" | cut -d= -f2)
 
     type=$(cat "$file" | jq -r ".[\"$key\"] | type")
 
-    echo "$key, $value, $type"
+    if [ "$type" = "string" ]; then
+        value=$(echo "$value" | sed 's/"/\\"/g')
+        value="\"$value\""
+    fi
 
+    configContent=$(cat "$config")
+    configContent=$(echo "$configContent" | sed "s|\${$key}|$value|g")
+
+    echo "$configContent" > $config
 
 done <<< "$variables"
 
-# java -jar ./keycloak-config-cli.jar
+java -jar ./keycloak-config-cli.jar
