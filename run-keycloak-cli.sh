239ccfaec5ab5f95466eafc586bdac8604ad6839
Connor Brady <connorbrady@codox.eu>
2023-04-08 13:59:29 +0100

    WIP Variable substitution

--- run-keycloak-cli.sh
+++ run-keycloak-cli.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Substitue environment variables
+# Extract user specified variables
+variables=$(jq -rj '. | to_entries[] | "\(.key)=\(.value)\n"' /misc/keycloak-cli-config.json)
 
+# printf $variables
 
-java -jar ./keycloak-config-cli.jar
+while IFS= read -r line; do
+    echo "Line: $line"
+done <<< "$variables"
+
+# java -jar ./keycloak-config-cli.jar
