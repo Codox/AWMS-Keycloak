98c127afd862617d3683b7ec8bedea28747b1311
Connor Brady <connorbrady@codox.eu>
2023-04-08 19:14:28 +0100

    UPDATED Bash script to separate key and values

--- run-keycloak-cli.sh
+++ run-keycloak-cli.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # Extract user specified variables
-variables=$(jq -rj '. | to_entries[] | "\(.key)=\(.value)\n"' /misc/keycloak-cli-config.json)
+file=/misc/keycloak-cli-config.json
+variables=$(jq -rj '. | to_entries[] | "\(.key)=\(.value)\n"' $file)
+
+echo $variables
 
 # printf $variables
 
 while IFS= read -r line; do
-    echo "Line: $line"
+    echo $line
+    key=$(echo "$line" | cut -d= -f1)
+    value=$(echo "$line" | cut -d= -f2)
+
+    type=$(cat "$file" | jq -r ".[\"$key\"] | type")
+
+    echo "$key, $value, $type"
+
+
 done <<< "$variables"
 
 # java -jar ./keycloak-config-cli.jar
